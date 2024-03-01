7386cd5959aedd5e11e75c6fb367139584c2bc9b
nsk1501 <nsk1501@naver.com>
2024-03-01 16:10:10 +0900

    Made blogname style

--- blog/src/App.js
+++ blog/src/App.js
@@ -2,11 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+   let post = '안양 회 맛집'
   return (
     <div className="App">
       <div className="black-nav">
-        <h4>승권이의 첫 블로그 만들기</h4>
+        <h4 style={ {color : 'yellow', fontSize : '16px'} } >승권이의 첫 블로그 만들기</h4>
       </div>
+      <h4>{ post }</h4>
+   
       </div>
   );
 }
