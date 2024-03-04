4c26d741aee517ce555512f65ee50579bed55253
nsk1501 <nsk1501@naver.com>
2024-03-04 17:52:34 +0900

    Use state function

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,14 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
 
-   let post = '안양 회 맛집'
-  return (
+   let post = '안양 맛집'
+   let [글제목, b] = useState(['안양 한식 추천','안양 횟집 추천','안양 일식 맛집']);
+   
+
+   return(
     <div className="App">
       <div className="black-nav">
-        <h4 style={ {color : 'yellow', fontSize : '16px'} } >승권이의 첫 블로그 만들기</h4>
+        <h4>React Blog</h4>
       </div>
+      <div className="list">
+        <h4>{ 글제목[0] }</h4>
+        <p>3월 1일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{ 글제목[1] }</h4>
+        <p>3월 4일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{ 글제목[2] }</h4>
+        <p>3월 4일 발행</p>
+      </div>
+      
       <h4>{ post }</h4>
    
       </div>
