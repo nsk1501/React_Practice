e40d33eba10b821f78209a62874f1637212f1041
nsk1501 <nsk1501@naver.com>
2024-03-04 18:17:59 +0900

    Made onClick

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,3 +1,5 @@
+/* eslint-disable*/
+
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
@@ -6,6 +8,8 @@ function App() {
 
    let post = '안양 맛집'
    let [글제목, b] = useState(['안양 한식 추천','안양 횟집 추천','안양 일식 맛집']);
+   let [따봉, 따봉변경] = useState(0);
+
    
 
    return(
@@ -14,15 +18,20 @@ function App() {
         <h4>React Blog</h4>
       </div>
       <div className="list">
-        <h4>{ 글제목[0] }</h4>
+        <h4>{ 글제목[0] } <span onClick={() => { 따봉변경(따봉 + 1) }}>👍</span>
+        {따봉} </h4>
         <p>3월 1일 발행</p>
+
+
       </div>
       <div className="list">
-        <h4>{ 글제목[1] }</h4>
+        <h4>{ 글제목[1] }  <span onClick={() => { 따봉변경(따봉 + 1) }}>👍</span>
+        {따봉} </h4>
         <p>3월 4일 발행</p>
       </div>
       <div className="list">
-        <h4>{ 글제목[2] }</h4>
+        <h4>{ 글제목[2] }   <span onClick={() => { 따봉변경(따봉 + 1) }}>👍</span>
+        {따봉} </h4>
         <p>3월 4일 발행</p>
       </div>
       
